a9751f07f38f7e8cc4e2712b87157a925a83aebf
matheusjean <matheusjean11@hotmail.com>
2023-10-12 18:44:50 -0300

    feat: get, update, delete services

--- src/services/categories.ts
+++ src/services/categories.ts
@@ -1,5 +1,22 @@
 import httpClient from "../http-client";
 import Categories from "../models/categories";
+import CategoryData from "../models/categoryData";
+
+interface CategoryRequest {
+  id?: string;
+  isActive?: boolean;
+}
+
+interface CategoriesInactivate {
+  data: [
+    {
+      id: string;
+      name: string;
+      is_active?: boolean;
+      created_at: Date;
+    }
+  ];
+}
 
 export const getAllCategories = async (): Promise<Categories | null> => {
   try {
@@ -13,4 +30,20 @@ export const getAllCategories = async (): Promise<Categories | null> => {
     console.error("Erro ao buscar categorias:", error);
     return null;
   }
+};
+
+export const getCategory = async (): Promise<CategoryData> => {
+  const category = (await httpClient.get(`category`)).data;
+  return category;
+};
+
+export const updateCategoryById = async (
+  categoryId: string,
+  data: CategoryRequest
+): Promise<CategoriesInactivate> => {
+  return (await httpClient.put(`category/${categoryId}`, data)).data;
+};
+
+export const deleteCategory = async (categoryId: string) => {
+  await httpClient.delete(`category/${categoryId}`);
 };
