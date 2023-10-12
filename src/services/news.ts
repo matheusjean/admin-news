40d90a56dd8ee55392401d901f9b5381f38969f8
matheusjean <matheusjean11@hotmail.com>
2023-10-11 21:40:32 -0300

    feat: add array to NewsData

--- src/services/news.ts
+++ src/services/news.ts
@@ -1,60 +1,60 @@
-import httpClient from '../http-client'
-import NewsForCreate from '../models/for-create/news'
-import News from '../models/news'
-import NewsData from '../models/newsData'
+import httpClient from "../http-client";
+import NewsForCreate from "../models/for-create/news";
+import News from "../models/news";
+import NewsData from "../models/newsData";
 
 interface NewsRequest {
-  id?: string
-  isActive?: boolean
+  id?: string;
+  isActive?: boolean;
 }
 
 interface NewsCreateUpdate {
-  hat: string
-  title: string
-  text: string
-  author: string
-  image: string
-  link: string
-  isActive: boolean
+  hat: string;
+  title: string;
+  text: string;
+  author: string;
+  image: string;
+  link: string;
+  isActive: boolean;
 }
 
 const getNews = async (newsId: string): Promise<News> => {
-  const news = (await httpClient.get(`news/${newsId}`)).data
+  const news = (await httpClient.get(`news/${newsId}`)).data;
 
-  return news
-}
+  return news;
+};
 
 const createNews = async (newNews: NewsForCreate) => {
-  const createNewsResponse = (await httpClient.post<any>(`news`, newNews)).data
+  const createNewsResponse = (await httpClient.post<any>(`news`, newNews)).data;
 
-  return createNewsResponse
-}
+  return createNewsResponse;
+};
 
-const getAllNews = async (): Promise<NewsData> => {
-  const news = await httpClient.get('news')
+const getAllNews = async (): Promise<NewsData[] | any> => {
+  const news = await httpClient.get("news");
 
-  return news
-}
+  return news;
+};
 
 const deleteNews = async (newsId: string) => {
-  await httpClient.delete(`news/${newsId}`)
-}
+  await httpClient.delete(`news/${newsId}`);
+};
 
 const updateNewsById = async (
   newsId: string,
   data: NewsRequest
 ): Promise<NewsData> => {
-  return (await httpClient.put(`news/${newsId}`, data)).data
-}
+  return (await httpClient.put(`news/${newsId}`, data)).data;
+};
 
 const updateNews = async (
   newsId: string,
   newsToBeUpdated: NewsCreateUpdate
 ): Promise<News> => {
-  const news = (await httpClient.put(`news/${newsId}`, newsToBeUpdated)).data
+  const news = (await httpClient.put(`news/${newsId}`, newsToBeUpdated)).data;
 
-  return news
-}
+  return news;
+};
 
 export {
   getNews,
@@ -62,5 +62,5 @@ export {
   getAllNews,
   deleteNews,
   updateNewsById,
-  updateNews
-}
+  updateNews,
+};
