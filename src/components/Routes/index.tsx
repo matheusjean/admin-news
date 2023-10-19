7288fb5fe60c5b32388d1919475e63ba3bd666dd
matheusjean <matheusjean11@hotmail.com>
2023-10-19 15:28:44 -0300

    feat: route with validation

--- src/components/Routes/index.tsx
+++ src/components/Routes/index.tsx
@@ -1,30 +1,46 @@
 import React from 'react'
-import { Switch, Route, RouteProps } from 'react-router-dom'
+import { Switch, Route, RouteProps, Redirect } from 'react-router-dom'
 
 import DefaultLayout from '../../components/Layouts/DefaultLayout'
+import { useAuth } from '../../context/AuthContext'
 import Category from '../../pages/Category'
 import CreateAndEditCategory from '../../pages/Category/CreateAndEdit'
+import Login from '../../pages/Login'
 import News from '../../pages/News'
 import CreateAndEditNews from '../../pages/News/CreateAndEdit'
+import AuthLayout from '../Layouts/AuthLayout'
 
-const Default = (props: RouteProps) => {
+const AuthRequired = (props: RouteProps) => {
+  const { user } = useAuth()
+
+  if (!user) {
+    return <Redirect to="/login" />
+  }
+
   return (
     <DefaultLayout>
       <Route {...props} />
     </DefaultLayout>
   )
 }
+
 export default function Routes() {
   return (
     <Switch>
-      <Default exact path="/" />
-      <Default exact path="/news" component={News} />
-      <Default exact path="/create-news" component={CreateAndEditNews} />
-      <Default exact path="/edit-news/:newsId" component={CreateAndEditNews} />
+      <Route path="/login" exact>
+        <AuthLayout>
+          <Login />
+        </AuthLayout>
+      </Route>
 
-      <Default exact path="/category" component={Category} />
-      <Default exact path="/create-category" component={CreateAndEditCategory} />
-      <Default exact path="/edit-category/:categoryId" component={CreateAndEditCategory} />
+      <AuthRequired exact path="/" />
+      <AuthRequired exact path="/news" component={News} />
+      <AuthRequired exact path="/create-news" component={CreateAndEditNews} />
+      <AuthRequired exact path="/edit-news/:newsId" component={CreateAndEditNews} />
+
+      <AuthRequired exact path="/category" component={Category} />
+      <AuthRequired exact path="/create-category" component={CreateAndEditCategory} />
+      <AuthRequired exact path="/edit-category/:categoryId" component={CreateAndEditCategory} />
     </Switch>
   )
 }
