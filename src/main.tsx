e9d4d6ff9c5bf37cb2ab46d5ca4d9c83245281ff
matheusjean <matheusjean11@hotmail.com>
2023-10-25 13:04:23 -0300

    chore: config for esm

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,25 @@
-import React from 'react'
-import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import { ThemeProvider } from 'styled-components'
+import { ThemeProvider } from 'styled-components';
 
-import App from './components/App'
+import App from './components/App';
 import AuthProvider from './context/AuthContext';
-import GlobalStyles from './styles/global'
-import theme from './styles/theme'
+import GlobalStyles from './styles/global';
+import theme from './styles/theme';
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+const root = document.getElementById("root");
 
-root.render(
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Router>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
       </Router>
     </ThemeProvider>
   </React.StrictMode>
-)
+);
